9f97486e77df2f390fc08cc675d8d8e21e227fe5
Adrian Mostowski <adaxelx@o2.pl>
2019-03-30 19:44:30 +0100

    Updates

---
+++ precache-manifest.16104a9fd2c1b3aac799284426c4324a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f2a42419d49fb4e17a0a58940e218b5",
+    "url": "/Strona-taty/static/media/triangles.2f2a4241.png"
+  },
+  {
+    "revision": "b12d3494c026a951cff0ff564db1e68a",
+    "url": "/Strona-taty/static/media/other.b12d3494.jpg"
+  },
+  {
+    "revision": "b5acc13d1fc27dec2952",
+    "url": "/Strona-taty/static/js/runtime~main.b5acc13d.js"
+  },
+  {
+    "revision": "3d6b6e6551d4c2d41645",
+    "url": "/Strona-taty/static/js/main.3d6b6e65.chunk.js"
+  },
+  {
+    "revision": "f35a961eac3985e2283f",
+    "url": "/Strona-taty/static/js/2.f35a961e.chunk.js"
+  },
+  {
+    "revision": "3d6b6e6551d4c2d41645",
+    "url": "/Strona-taty/static/css/main.48f7e3e9.chunk.css"
+  },
+  {
+    "revision": "a6ea3bcef72b3b0325bc296f71a5180b",
+    "url": "/Strona-taty/index.html"
+  }
+];
