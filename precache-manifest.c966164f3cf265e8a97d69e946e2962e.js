77111163da99135c8845b95743ff6bdc0723a11d
Adrian Mostowski <adaxelx@o2.pl>
2019-03-30 20:32:21 +0100

    Updates

---
+++ precache-manifest.c966164f3cf265e8a97d69e946e2962e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f2a42419d49fb4e17a0a58940e218b5",
+    "url": "/react-with-router/static/media/triangles.2f2a4241.png"
+  },
+  {
+    "revision": "b12d3494c026a951cff0ff564db1e68a",
+    "url": "/react-with-router/static/media/other.b12d3494.jpg"
+  },
+  {
+    "revision": "8b22acc1a0208fd3497a",
+    "url": "/react-with-router/static/js/runtime~main.8b22acc1.js"
+  },
+  {
+    "revision": "5f97c8101f7eaea435db",
+    "url": "/react-with-router/static/js/main.5f97c810.chunk.js"
+  },
+  {
+    "revision": "7630983d6804d6d675b8",
+    "url": "/react-with-router/static/js/2.7630983d.chunk.js"
+  },
+  {
+    "revision": "5f97c8101f7eaea435db",
+    "url": "/react-with-router/static/css/main.5fd58c7f.chunk.css"
+  },
+  {
+    "revision": "6cfaf09322bc4b11874f944c3131b2af",
+    "url": "/react-with-router/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Strona-taty/precache-manifest.16104a9fd2c1b3aac799284426c4324a.js"
+  "/react-with-router/precache-manifest.c966164f3cf265e8a97d69e946e2962e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Strona-taty/index.html", {
+workbox.routing.registerNavigationRoute("/react-with-router/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
